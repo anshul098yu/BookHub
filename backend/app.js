4fa71d0a1ec2824deb80e289c38944bbc0eb42fb
Harsh Sinha <121681112+anshul098yu@users.noreply.github.com>
2025-11-13 22:10:54 +0530

    Add new client URL to CORS middleware

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ cloudinary.config({
 // Middleware
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://book-hub-zeta-blue.vercel.app"],
+    origin: [process.env.CLIENT_URL, "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://book-hub-zeta-blue.vercel.app" ," https://book-kjc538idh-anshul098yus-projects.vercel.app "],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
